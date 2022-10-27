2dbd10dfc7f0488ad4bf88a11731e3c3caf6deed
Mex de Loo <mex@zeodev.cc>
2022-10-27 07:51:51 +0200

    fix: null check

--- app/Http/Resources/ZeurMeisterResource.php
+++ app/Http/Resources/ZeurMeisterResource.php
@@ -18,7 +18,7 @@ class ZeurMeisterResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            'name' => $this->resource->user->name,
+            'name' => $this->resource ? $this->resource->user->name : null,
         ];
     }
 }
