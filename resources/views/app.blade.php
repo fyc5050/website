bc582d5d238d3bd534dd2caac85bd5125355d04c
Mex de Loo <mex@zeodev.cc>
2022-12-16 14:04:38 +0100

    update: logo and favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
+    <link rel="icon" type="image/png" href="{{ \Vite::asset('resources/images/logo.png') }}"/>
+
     <title>{{ config('app.name') }}</title>
 
     <!-- Fonts -->
