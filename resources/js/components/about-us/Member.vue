7e126ebc50a6504c54c2461f3076682bee828eac
Mex de Loo <mex@zeodev.cc>
2023-05-28 20:46:41 +0200

    fix: mobile layout

--- resources/js/components/about-us/Member.vue
+++ resources/js/components/about-us/Member.vue
@@ -1,26 +1,28 @@
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-6 gap-8">
+    <div class="grid grid-cols-1 md:grid-cols-6 gap-4 md:gap-8">
         <img class="md:col-span-3 bg-white rounded-md w-full h-auto" :src="props.image"/>
 
         <div class="md:col-span-3" :class="props.reverseLayout && 'md:-order-1'">
-            <div>
-                <h4 class="text-white font-bold text-2xl flex">
+            <div class="flex justify-between flex-col md:flex-row">
+                <h4 class="text-white font-bold text-2xl">
                     {{ props.name }}
+                </h4>
 
-                    <span class="ml-4 bg-primary-500 bg-opacity-30 text-xs text-primary-500 py-2 px-3 rounded-full">
+                <div class="space-x-4 mt-2 mb-1 md:mb-0 md:mt-0">
+                    <span class="bg-primary-500 bg-opacity-30 text-xs text-primary-500 py-2 px-3 rounded-full">
                         {{ props.function }}
                     </span>
 
-                    <span class="ml-4 bg-yellow-500 bg-opacity-30 text-xs text-yellow-500 py-2 px-3 rounded-full">
+                    <span class="bg-yellow-500 bg-opacity-30 text-xs text-yellow-500 py-2 px-3 rounded-full">
                         <font-awesome-icon icon="fas fa-beer-mug" class="w-4 h-4 text-yellow-500 mr-2"/>
                         {{ props.desCount }}
                     </span>
-                </h4>
-
-                <p class="text-gray-300 mt-4">
-                    <slot name="text"/>
-                </p>
+                </div>
             </div>
+
+            <p class="text-gray-300 mt-4">
+                <slot name="text"/>
+            </p>
         </div>
     </div>
 </template>
